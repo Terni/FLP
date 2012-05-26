b208b45503dcd202f5635bad3e588e23653a3961
Tomas P <xprokopt@centrum.cz>
2012-05-26 15:02:22 +0200

    first commit

--- binaryTree.hs
+++ binaryTree.hs
@@ -52,11 +52,72 @@ treeRemoveElem x (Node a left right)
   | x < a = Node a (treeRemoveElem x left) right
   | x > a = Node a left (treeRemoveElem x right)
   | x == a && treeEmpty left && treeEmpty right = Empty
-  | x == a && treeEmpty left && treeNotEmpty right = right
-  | x == a && treeNotEmpty left && treeEmpty right = left
-  | otherwise = Node (mostLeft right) left (treeRemoveElem (mostLeft right) right)
+  | x == a && treeEmpty left && treeNotEmpty right = right -- jestli ze je leva strana prazdna a prave ne tak vracim pravou
+  | x == a && treeNotEmpty left && treeEmpty right = left -- jestli je leva neprazdna a prava prazdna tak vracim levou
+  | otherwise = Node (mostLeft right) left (treeRemoveElem (mostLeft right) right) -- pro oba dva neprazdne
       where mostLeft (Node b l r) = if treeEmpty l then b else mostLeft l
 -- treeRemoveElem 0 (Node 0 (Empty) (Empty))
+-- treeRemoveElem (-2) (Node 0 (Node (-1) (Node (-2) Empty Empty) Empty) (Node 1 Empty (Node 2 (Node 3 Empty Empty) (Node 4 Empty Empty))))
+
+--        0
+--     /    \
+--   -1       1
+--   / \     / \
+-- -2  E    E  2
+-- /\         / \
+-- E E       3  4
+--              / \
+--             E  E
+
+
+-- poziti: Hlavni_strom heldany_strom = vysledny_strom
+treeChangeValue :: (Ord a, Num a) => BTree a -> BTree a -> BTree a
+treeChangeValue Empty Empty = Empty
+treeChangeValue Empty _ = Empty -- protoze neexistuje hlavni strom
+treeChangeValue mainTree Empty =  mainTree
+treeChangeValue (Node a al ar) (Node b bl br)
+  | b < a = Node a (treeChangeValue al b_all) ar -- jdi do leva
+  | b > a = Node a al (treeChangeValue ar b_all) -- jdi do prava
+  | a == b && treeEmpty bl && treeEmpty br = treeUpValue (Node a al ar)
+  | a == b && treeNotEmpty bl && treeEmpty br = (Node newValue (treeChangeValue al bl) ar)
+  | a == b && treeEmpty bl && treeNotEmpty br = (Node newValue al (treeChangeValue ar br))
+  | a == b && treeNotEmpty bl && treeNotEmpty br = (Node newValue (treeChangeValue al bl) (treeChangeValue ar br))
+--   | a == b && treeNotEmpty al && treeNotEmpty ar && treeNotEmpty bl && treeEmpty br  = (Node newValue (treeChangeValue al bl) (treeUpValue ar))
+--   | a == b && treeNotEmpty al && treeNotEmpty ar && treeEmpty bl && treeNotEmpty br  = (Node newValue (treeUpValue al) (treeChangeValue ar br))
+  where
+    b_all = (Node b bl br)
+--     a_all = (Node a al ar)
+    newValue = if a >= 0 then a + 1 else a - 1
+   
+   
+--- TESTY   
+-- treeChangeValue (Node 0 (Node (-1) ((Node (-2) (Node (-3) Empty Empty) (Node (-4) Empty Empty))) Empty) (Node 1 Empty Empty)) (Node (-2) (Node (-3) Empty Empty) (Node (-4) Empty Empty))
+   
+-- treeChangeValue (Node 0 (Node (-1) Empty Empty) (Node 1 Empty Empty)) (Node 0 (Node (-1) Empty Empty) (Node 1 Empty Empty))
+
+-- treeChangeValue (Node 0 (Node (-1) ((Node (-2) Empty (Node (-4) Empty Empty))) Empty) Empty)          (Node 0  Empty Empty)  
+-- treeChangeValue (Node 0 (Node (-1) (Node (-2) Empty Empty) Empty) Empty)                              (Node (-1) (Node (-2) Empty Empty) Empty)
+
+-- treeChangeValue (Node 0 (Node (-1) ((Node (-2) (Node (-3) Empty Empty) (Node (-4) Empty Empty))) Empty) (Node 1 Empty Empty))      (Node 0 Empty Empty)
+-- treeChangeValue (Node 0 (Node (-1) ((Node (-2) (Node (-3) Empty Empty) (Node (-4) Empty Empty))) Empty) (Node 1 Empty Empty))      (Node 1 Empty Empty)    
+-- treeChangeValue (Node 0 (Node (-1) ((Node (-2) (Node (-3) Empty Empty) (Node (-4) Empty Empty))) Empty) (Node 1 Empty Empty))      (Node 0 Empty (Node 1 Empty Empty))
+--         0
+--      /    \
+--    -1       1
+--    / \     / \
+--  -2  E    E  E
+--  / \        
+-- -3 -4      
+ 
+treeUpValue :: (Ord a, Num a) => BTree a -> BTree a
+treeUpValue (Node a left right)
+  | treeEmpty (Node a left right) = Empty
+  | treeEmpty left && treeEmpty right = (Node newValue left right)
+  | treeEmpty left && treeNotEmpty right = (Node newValue left (treeUpValue right))
+  | treeNotEmpty left && treeEmpty right = (Node newValue (treeUpValue left) right)
+  | otherwise = (Node newValue (treeUpValue left) (treeUpValue right)) -- pruchod preorder kdyz oba pravky nejsou prazdne
+  where
+    newValue = if a >= 0 then a + 1 else a - 1
 
 
 treePathToElem :: (Ord a) => a -> BTree a -> [a]
@@ -99,3 +160,4 @@ treeInorder x = ino x
 -- [0,1,2,3]
 
    
+-- treePreorder (Node 0 (Node (-1) (Node (-2) Empty Empty) Empty) (Node 1 Empty (Node 2 Empty (Node 3 Empty Empty))))
